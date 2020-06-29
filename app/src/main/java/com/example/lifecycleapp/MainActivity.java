2a3dbd362e26c64a6be2b84f118f142de20c0a2d
sr <candy871121@gmail.com>
2020-06-29 10:50:30 +0800

    完成

--- app/src/main/java/com/example/lifecycleapp/MainActivity.java
+++ app/src/main/java/com/example/lifecycleapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.lifecycleapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,10 +29,57 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(this, "onCreate(1)", Toast.LENGTH_LONG).show();
     }
+    @Override
+    protected  void onStart() {
+        super.onStart();
+        Toast.makeText(this,"onStart(1)",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"onResume(1)",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"onRestart(1)",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"onPause(1)",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"onStop(1)",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"onDestroy(1)",Toast.LENGTH_LONG).show();
+    }
+
     private Button.OnClickListener myListener = new Button.OnClickListener() {
         @Override
         public void onClick(View v) {
-
+            switch (v.getId()){
+                case R.id.btnDial:
+                    Uri uri=Uri.parse("tel:0999123456");
+                    Intent intent = new Intent(Intent.ACTION_DIAL,uri);
+                    startActivity(intent);
+                    break;
+                case R.id.btnSecond:
+                    Intent intent1 = new Intent(MainActivity.this,SecondActivity.class);
+                    startActivity(intent1);
+                    break;
+                case R.id.btnFinish:
+                   finish();
+                    break;
+            }
         }
+
+
+
     };
 }
